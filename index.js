d7a0ade288435b19b982fd79d5acf4f3d4c37388
Simon Degraeve <simon.degraeve@gmail.com>
2014-11-06 08:27:37 +0800

    Initial release

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+ * node-focuspoint
+ * https://github.com/SimonDegraeve/node-focuspoint
+ *
+ * Copyright 2014, Simon Degraeve
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = require('./lib/focuspoint');
